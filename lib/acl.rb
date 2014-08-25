530e446885abfb76d562001d3c211342071d5de9
Kyle Crawford <kcrwfrd@gmail.com>
2014-08-25 17:51:20 -0400

    Make it read acl entries

--- lib/acl.rb
+++ lib/acl.rb
@@ -1,6 +1,54 @@
 require "acl/version"
 
 module OSX
+
   class ACL
+
+    attr_accessor :path
+
+    def self.of(path)
+      new.tap {|acl| acl.path = path }
+    end
+
+    def entries
+      entry_lines.map {|line| ACL_Entry.from_text(line) }
+    end
+
+    def entry_lines
+      file_descriptor = File.open(path, "r")
+      acl_ptr = api.acl_get_fd(file_descriptor.fileno)
+      acl_text_ptr = api.acl_to_text(acl_ptr, nil)
+      return [] if acl_text_ptr.null?
+      ace_lines = acl_text_ptr.read_string.split("\n")[1..-1]
+      ace_lines
+    ensure
+      api.acl_free(acl_text_ptr)
+      api.acl_free(acl_ptr)
+      file_descriptor.close
+    end
+
+    def api
+      ACL_API
+    end
+
+  end
+
+  class ACL_Entry
+    attr_accessor :line
+
+    def self.from_text(line)
+      new.tap {|entry| entry.line = line }
+    end
+
+  end
+
+  require 'ffi'
+  module ACL_API
+    extend FFI::Library
+    ffi_lib FFI::Library::LIBC
+    attach_function :acl_get_fd, [:int], :pointer
+    attach_function :acl_to_text, [:pointer, :pointer], :pointer
+    attach_function :acl_valid, [:pointer], :int
+    attach_function :acl_free, [:pointer], :int
   end
 end
--- spec/acl_spec.rb
+++ spec/acl_spec.rb
@@ -1,11 +1,46 @@
 require 'spec_helper'
 
-describe OSX::ACL do
+include OSX
+
+def make_dir_with_two_aces
+  system(
+    "
+      rm -Rf 'spec/fixtures/dir_with_two_aces';
+      mkdir -p 'spec/fixtures/dir_with_two_aces';
+      chmod +a 'group:staff allow read' 'spec/fixtures/dir_with_two_aces';
+      chmod +a 'group:_www allow read' 'spec/fixtures/dir_with_two_aces';
+    "
+  )
+end
+
+describe ACL do
   it 'has a version number' do
-    expect(OSX::ACL::VERSION).not_to be nil
+    expect(ACL::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can be intantiated from a path' do
+    expect(ACL.of("/")).to be_kind_of(ACL)
+    expect(ACL.of("/tmp").path).to eq("/tmp")
+  end
+
+  it "can read lines of acl entries" do
+    expect(ACL.of("/").entry_lines).to be_kind_of(Array)
+    make_dir_with_two_aces
+    expect(ACL.of("spec/fixtures/dir_with_two_aces").entry_lines.length).to eq(2)
+    expect(ACL.of("spec/fixtures/dir_with_two_aces").entry_lines.first).to eq("group:ABCDEFAB-CDEF-ABCD-EFAB-CDEF00000046:_www:70:allow:read")
+    expect(ACL.of("spec/fixtures/dir_with_two_aces").entry_lines.last).to eq("group:ABCDEFAB-CDEF-ABCD-EFAB-CDEF00000014:staff:20:allow:read")
+  end
+
+  describe "its entries" do
+    it 'is an array' do
+      expect(ACL.of("/").entries).to be_kind_of(Array)
+    end
+
+    it 'returns entry instances' do
+      make_dir_with_two_aces
+      entries = ACL.of("spec/fixtures/dir_with_two_aces").entries
+      expect(entries.length).to eq(2)
+      expect(entries.first).to be_kind_of(ACL_Entry)
+    end
   end
 end
